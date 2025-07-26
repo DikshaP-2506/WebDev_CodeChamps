c3bf6add9ee71b1de8e22a90198cd12215a19d4f
Sakshi Thorat <Sakshi Thorat@Sakshi.(none)>
2025-07-26 15:54:24 +0530

    auth

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -4,8 +4,8 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
-import VendorLogin from "./pages/auth/VendorLogin";
-import SupplierLogin from "./pages/auth/SupplierLogin";
+import VendorAuth from "./pages/auth/VendorAuth";
+import SupplierAuth from "./pages/auth/SupplierAuth";
 import VendorDashboard from "./pages/vendor/VendorDashboard";
 import SupplierDashboard from "./pages/supplier/SupplierDashboard";
 import SupplierDetails from "./pages/vendor/SupplierDetails";
@@ -23,8 +23,8 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Landing />} />
-          <Route path="/vendor/login" element={<VendorLogin />} />
-          <Route path="/supplier/login" element={<SupplierLogin />} />
+          <Route path="/vendor/login" element={<VendorAuth />} />
+          <Route path="/supplier/login" element={<SupplierAuth />} />
           <Route path="/vendor/dashboard" element={<VendorDashboard />} />
           <Route path="/vendor/supplier/:supplierId" element={<SupplierDetails />} />
           <Route path="/vendor/order-summary" element={<OrderSummary />} />
